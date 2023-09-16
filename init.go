1c21984215de7846e07db21bbc59601117fbb743
Saeed Yazdinezhad <saeedyazdinezhad@gmail.com>
2023-09-16 02:58:21 -0700

    init logging

--- init.go
+++ init.go
@@ -2,8 +2,11 @@ package subspace_experiment
 
 import (
 	"net/http"
+
+	"github.com/xm0onh/subspace_experiment/log"
 )
 
 func Init() {
+	log.Setup()
 	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 1000
 }
--- server/server.go
+++ server/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/xm0onh/subspace_experiment"
+	"github.com/xm0onh/subspace_experiment/log"
 )
 
 func main() {
 	subspace_experiment.Init()
+	log.Infof("node %v starting...", 1)
 
 }
