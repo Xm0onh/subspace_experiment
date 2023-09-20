66222bb5dd7c33c40d46663a26d2d93d37fb60f3
Saeed Yazdinezhad <saeedyazdinezhad@gmail.com>
2023-09-20 00:01:08 -0700

    add subspace policy for mempool and tx bundles

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -2,16 +2,15 @@ package mempool
 
 import (
 	"math"
-	"math/big"
 	"math/rand"
 )
 
 type Mempool struct {
 	txns             []Transaction
-	uMax             *big.Int
+	uMax             uint64
 	expected_bundle  int
 	actual_bundle    int
-	current_tx_range int
+	current_tx_range uint64
 }
 
 type Transaction struct {
@@ -22,7 +21,12 @@ type Transaction struct {
 	Seq    int
 }
 
-const lettersAndNumbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const (
+	lettersAndNumbers         = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	INITIAL_TX_RANGE_DIVISOR  = 3 // U256::MAX/3
+	TX_RANGE_ADJUSTMENT_RATIO = 4 // maximum or minimum ratio for adjustment
+	MAX                       = 2 ^ 20
+)
 
 func randStringBytes(n int) string {
 	b := make([]byte, n)
@@ -57,9 +61,29 @@ func transactionGenerator(numberOfTransactions int) []Transaction {
 
 func NewMemPool() *Mempool {
 	return &Mempool{
-		txns: transactionGenerator(500),
-		uMax: new(big.Int).Exp(big.NewInt(2), big.NewInt(64), nil),
+		txns:             transactionGenerator(int(MAX / 3)),
+		uMax:             MAX,
+		current_tx_range: MAX,
 	}
+}
+
+func (m *Mempool) BidirectionalDistance(hash1, hash2 string) int {
+	// TODO:
+	return 0
+}
+
+func (m *Mempool) SelectTransactionsForBundle(vrfSignature string) []Transaction {
+	// slot_vrf_hash := hash(vrfSignature)  // TODO: implement a hash function
+	selectedTxs := []Transaction{}
+
+	// for _, tx := range m.txns {
+	// 	distance := m.BidirectionalDistance("0", tx.Hash)
+	// 	if distance <= m.current_tx_range/2 {
+	// 		selectedTxs = append(selectedTxs, tx)
+	// 	}
+	// }
+
+	return selectedTxs
 }
 
 func (m *Mempool) GetTx() []Transaction {
@@ -69,5 +93,5 @@ func (m *Mempool) GetTx() []Transaction {
 func (m *Mempool) TxRangeAdjustment() {
 	newRange := math.Max(
 		math.Min(float64(m.expected_bundle)/float64(m.actual_bundle), 4), 0.25) * float64(m.current_tx_range)
-	m.current_tx_range = int(newRange)
+	m.current_tx_range = uint64(newRange)
 }
